f3e22bf2a70a6bac8207a3843de7bcf5d73082bc
Dmitry V. Levin <ldv@altlinux.org>
2022-02-10 08:00:00 +0000

    Import parts of cc_compat.h and macros.h from the strace project
    This is primarily for a safe implementation of ARRAY_SIZE macro,
    but other use cases may come up later.
    
    Although the portability glue is not needed in this project, it has
    not been removed to simplify potential synchronization of these files
    in the future.

---
+++ hasher-priv/cc_compat.h
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2015 Dmitry V. Levin <ldv@altlinux.org>
+ * Copyright (c) 2015-2022 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef HASHER_CC_COMPAT_H
+# define HASHER_CC_COMPAT_H
+
+# if defined __GNUC__ && defined __GNUC_MINOR__
+#  define GNUC_PREREQ(maj, min)	\
+	((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+# else
+#  define GNUC_PREREQ(maj, min)	0
+# endif
+
+# if defined __clang__ && defined __clang_major__ && defined __clang_minor__
+#  define CLANG_PREREQ(maj, min)	\
+	((__clang_major__ << 16) + __clang_minor__ >= ((maj) << 16) + (min))
+# else
+#  define CLANG_PREREQ(maj, min)	0
+# endif
+
+# if !(GNUC_PREREQ(2, 0) || CLANG_PREREQ(1, 0))
+#  define __attribute__(x)	/* empty */
+# endif
+
+# if GNUC_PREREQ(2, 5)
+#  define ATTRIBUTE_NORETURN	__attribute__((__noreturn__))
+# else
+#  define ATTRIBUTE_NORETURN	/* empty */
+# endif
+
+# if GNUC_PREREQ(2, 7)
+#  define ATTRIBUTE_FORMAT(args)	__attribute__((__format__ args))
+# else
+#  define ATTRIBUTE_FORMAT(args)	/* empty */
+# endif
+
+/*
+ * Evaluates to:
+ * 1, if the given two types are known to be the same;
+ * 0, otherwise.
+ */
+# if GNUC_PREREQ(3, 0)
+#  define IS_SAME_TYPE(x_, y_)						\
+	__builtin_types_compatible_p(__typeof__(x_), __typeof__(y_))
+# else
+/* Cannot tell whether these types are the same.  */
+#  define IS_SAME_TYPE(x_, y_)	0
+# endif
+
+# if GNUC_PREREQ(3, 0)
+#  define ATTRIBUTE_MALLOC	__attribute__((__malloc__))
+# else
+#  define ATTRIBUTE_MALLOC	/* empty */
+# endif
+
+# if GNUC_PREREQ(3, 1)
+#  define ATTRIBUTE_NOINLINE	__attribute__((__noinline__))
+# else
+#  define ATTRIBUTE_NOINLINE	/* empty */
+# endif
+
+# if GNUC_PREREQ(4, 0)
+#  define ATTRIBUTE_SENTINEL	__attribute__((__sentinel__))
+# else
+#  define ATTRIBUTE_SENTINEL	/* empty */
+# endif
+
+# if GNUC_PREREQ(4, 1)
+#  define ALIGNOF(t_)	__alignof__(t_)
+# else
+#  define ALIGNOF(t_)	(sizeof(struct { char x_; t_ y_; }) - sizeof(t_))
+# endif
+
+# if GNUC_PREREQ(4, 3)
+#  define ATTRIBUTE_ALLOC_SIZE(args)	__attribute__((__alloc_size__ args))
+# else
+#  define ATTRIBUTE_ALLOC_SIZE(args)	/* empty */
+# endif
+
+# if GNUC_PREREQ(7, 0)
+#  define ATTRIBUTE_FALLTHROUGH	__attribute__((__fallthrough__))
+# else
+#  define ATTRIBUTE_FALLTHROUGH	((void) 0)
+# endif
+
+#endif /* !HASHER_CC_COMPAT_H */
---
+++ hasher-priv/macros.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2001-2022 The strace developers.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef HASHER_MACROS_H
+# define HASHER_MACROS_H
+
+# include "cc_compat.h"
+
+/*
+ * Evaluates to:
+ * a syntax error, if the argument is 0;
+ * 0, otherwise.
+ */
+# define FAIL_BUILD_ON_ZERO(e_)	(sizeof(int[-1 + 2 * !!(e_)]) * 0)
+
+/*
+ * Evaluates to:
+ * 1, if the given type is known to be a non-array type;
+ * 0, otherwise.
+ */
+# define IS_NOT_ARRAY(a_)	IS_SAME_TYPE((a_), &(a_)[0])
+
+/*
+ * Evaluates to:
+ * a syntax error, if the argument is not an array;
+ * 0, otherwise.
+ */
+# define MUST_BE_ARRAY(a_)	FAIL_BUILD_ON_ZERO(!IS_NOT_ARRAY(a_))
+
+/* Evaluates to the number of elements in the specified array.  */
+# define ARRAY_SIZE(a_)	(sizeof(a_) / sizeof((a_)[0]) + MUST_BE_ARRAY(a_))
+
+#endif /* !HASHER_MACROS_H */
